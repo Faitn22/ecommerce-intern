493e15e66d0da958d806eec836be43123539db50
Your Name <you@example.com>
2024-07-15 13:18:11 +0700

    initial commit

---
+++ src/app/(pages)/create-account/page.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Gutter } from '../../_components/Gutter'
+import { RenderParams } from '../../_components/RenderParams'
+import { getMeUser } from '../../_utilities/getMeUser'
+import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
+import CreateAccountForm from './CreateAccountForm'
+
+import classes from './index.module.scss'
+
+export default async function CreateAccount() {
+  await getMeUser({
+    validUserRedirect: `/account?warning=${encodeURIComponent(
+      'Cannot create a new account while logged in, please log out and try again.',
+    )}`,
+  })
+
+  return (
+    <section className={classes.createAccount}>
+
+
+      <div className={classes.formWrapper}>
+        <Link href="/">
+          <Image
+            src="/img/logo.webp"
+            alt="logo"
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+
+          <div className={classes.formTitle}>
+            <h3>Create Account</h3>
+          </div>
+
+          <CreateAccountForm />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'Account',
+  description: 'Create an account or log in to your existing account.',
+  openGraph: mergeOpenGraph({
+    title: 'Account',
+    url: '/account',
+  }),
+}
